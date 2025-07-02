48838369eebd89347b49c7b2995653ec4fe92a46
Nikhitha1109 <pothulanikhitha@gmail.com>
2025-07-02 18:16:07 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,46 @@
+let valueName = true;
+console.log(valueName);
+console.log(typeof(valueName));
+let name = "nikki";
+console.log(name);
+console.log(typeof(name));
+var n = 90;
+console.log(n);
+console.log(typeof(n));
+let key;
+console.log(key);
+let p = null;
+console.log(p);
+let a = ["red", "green", "blue"];
+console.log(typeof(a));
+console.log(a);
+console.log(a[0]);
+function animal() {
+    console.log("humans are not animals");
+}
+console.log(typeof(animal));
+animal();
+let Education = {
+    name:"nikhitha",
+    age:16,
+    class:10,
+    section:"a",
+    goal:"Software Engineer",
+    schorlship:true,
+    cgpa:9.8,
+    favsubject:"maths",
+}
+console.log(Education);
+console.log(typeof(Education));
+let num=1;
+function outofstock(){
+    console.log(num);
+    num=num+1;
+    //alert("shoe is out of stock");
+}
+let timeout = setTimeout(outofstock,3000);
+var product=document.getElementById('shoe');
+product=SharedWorker;
+function submitFunction(){
+    console.log("button clicked");
+}
